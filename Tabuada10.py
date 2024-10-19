827ef23d3a4adbb53b1a63b380dfee3d18a08617
Fernando França Araujo <fernandofaraujo25@gmail.com>
2024-10-19 16:50:19 -0300

    Create Tabuada10.py

---
+++ Tabuada10.py
@@ -0,0 +1,17 @@
+NumeroTabuada = int(input("\nDigite aqui o número à ser calculado: "))
+
+print ("-"*15)
+
+print (f"\nAqui está a tabuada de {NumeroTabuada}:\n")
+print (f"{NumeroTabuada} x 1 = {NumeroTabuada * 1}")
+print (f"{NumeroTabuada} x 2 = {NumeroTabuada * 1}")
+print (f"{NumeroTabuada} x 3 = {NumeroTabuada * 1}")
+print (f"{NumeroTabuada} x 4 = {NumeroTabuada * 1}")
+print (f"{NumeroTabuada} x 5 = {NumeroTabuada * 1}")
+print (f"{NumeroTabuada} x 6 = {NumeroTabuada * 1}")
+print (f"{NumeroTabuada} x 7 = {NumeroTabuada * 1}")
+print (f"{NumeroTabuada} x 8 = {NumeroTabuada * 1}")
+print (f"{NumeroTabuada} x 9 = {NumeroTabuada * 1}")
+print (f"{NumeroTabuada} x 10 = {NumeroTabuada * 1}\n")
+
+print ("-"*15)
